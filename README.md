dfce1c95716fffe5ffebfc1806821988f1fc4133
Matheus <matheus@pop-os.localdomain>
2021-07-30 15:52:26 -0300

    feat: finished the course and added a README

---
+++ README.md
@@ -0,0 +1,3 @@
+# ENG: Blockchain with Node.js: Build your Blockchain | PT-BR: Blockchain com Node.js: Contrua a sua Blockchain
+
+[Course](https://www.udemy.com/course/blockchain-com-nodejs-contrua-a-sua-blockchain/)
--- src/blockchain.test.js
+++ src/blockchain.test.js
@@ -2,10 +2,12 @@ const Blockchain = require('./blockchain')
 const Block = require('./block')
 
 describe('Blockchain class', () => {
-  let bc;
+  let bc
+  let secondBc
 
   beforeEach(() => {
     bc = new Blockchain()
+    secondBc = new Blockchain()
   })
 
   it ('Should start with genesis block', () => {
@@ -17,5 +19,38 @@ describe('Blockchain class', () => {
     bc.addBlock(data)
 
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data)
+  })
+
+  it ('Should validate a valid chain', () => {
+    secondBc.addBlock('1500$')
+
+    expect(bc.isValidChain(secondBc.chain)).toBe(true)
+  })
+
+  it ('Should invalidate a chain with a corrupt genesis block', () => {
+    secondBc.chain[0].data = '0$'
+
+    expect(bc.isValidChain(secondBc.chain)).toBe(false)
+  })
+
+  it ('Should invalidate a corrupt chain', () => {
+    secondBc.addBlock('2000$')
+    secondBc.chain[1].data = '00$'
+
+    expect(bc.isValidChain(secondBc.chain)).toBe(false)
+  })
+
+  it ('Should replace the chain with a valid chain', () => {
+    secondBc.addBlock('450$')
+    bc.replaceChain(secondBc.chain)
+
+    expect(bc.chain).toEqual(secondBc.chain)
+  })
+
+  it ('Should not replace the chain with another chain with less or equal blocks', () => {
+    bc.addBlock('320$')
+    bc.replaceChain(secondBc.chain)
+
+    expect(bc.chain).not.toEqual(secondBc.chain)
   })
 })
